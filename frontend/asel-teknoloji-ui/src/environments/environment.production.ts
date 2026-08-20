0988bc8c001e871357ddb4fb39a0bc5950fb806f
Onur Altıntaş <onur40altintas@gmail.com>
2026-08-20 22:11:31 +0300

    config: sync recaptcha production site key

--- frontend/asel-teknoloji-ui/src/environments/environment.production.ts
+++ frontend/asel-teknoloji-ui/src/environments/environment.production.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://aselteknoloji.net/api',
   siteUrl: 'https://aselteknoloji.net',
   ga4Id: 'G-Q1DNE4B6Z9',
-  recaptchaSiteKey: '6Lfgr40tAAAAAJe6yhsAXs3TE5SnQanZzc2AbN7r'
+  recaptchaSiteKey: '6LcIQZAtAAAAALCXkpXjeLCwY020wlZswlHhGSjW'
 };
